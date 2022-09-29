b0e8e7de2712f56832647237096486a0ca91f5ea
iprogrammer1390 <iprogrammer1390@gmail.com>
2022-09-30 00:05:42 +0600

    Добавлено первое view с отображением всех записей

--- src/notes/views.py
+++ src/notes/views.py
@@ -1,3 +1,10 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import Note
 
-# Create your views here.
+def note_list_view(request):
+    notes = Note.objects.all()
+    context={
+        'notes':notes,
+    }
+    return render(request, 'note_list.html', context)
